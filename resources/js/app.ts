f98d9659088b5a01ff701dd325b33b22f0863463
Md. Anichur Rahaman <38912435+anis3139@users.noreply.github.com>
2023-01-08 20:26:04 +0600

    title dynamic

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -4,18 +4,15 @@ import "./bootstrap";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { createInertiaApp, Link } from "@inertiajs/inertia-vue3";
+import { createInertiaApp, Link, usePage } from "@inertiajs/inertia-vue3";
 import { InertiaProgress } from "@inertiajs/progress";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { createApp, h } from "vue";
 import { ZiggyVue } from "../../vendor/tightenco/ziggy/dist/vue.m";
 library.add(fas);
 
-const appName =
-    window.document.getElementsByTagName("title")[0]?.innerText || "Laravel";
-
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => `${title} - ${usePage().props.value?.global?.options?.site_name}`,
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.vue`,
