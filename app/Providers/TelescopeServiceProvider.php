2e2b18169e71b190ee02e1a03943c9571ae17077
Md. Anichur Rahaman <38912435+anis3139@users.noreply.github.com>
2023-01-09 11:35:24 +0600

    Update TelescopeServiceProvider.php

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -63,9 +63,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewTelescope', function ($user) {
-            return in_array($user->email, [
-                //
-            ]);
+            return $user->hasRole(['superadmin', 'admin']);
         });
     }
 }
