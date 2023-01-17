47b1023b2308171a3bcee396a9812023550adc32
Md. Anichur Rahaman <38912435+anis3139@users.noreply.github.com>
2023-01-17 18:46:14 +0600

    Header Added

--- app/Notifications/ResetPasswordNotification.php
+++ app/Notifications/ResetPasswordNotification.php
@@ -9,4 +9,17 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 class ResetPasswordNotification extends ResetPassword implements ShouldQueue
 {
     use Queueable;
+
+
+    /**
+     * @TODO
+     * Add This Construct for Seinding Post Mark  smtp Mail
+     * if you are not interast with post mark you can remove this constact method
+     */
+    public function __construct()
+    {
+        $this->callbacks[] =(function ($message) {
+            $message->getHeaders()->addTextHeader('X-PM-Message-Stream', 'Transactional');
+        });
+    }
 }
--- app/Notifications/VerifyEmailQueued.php
+++ app/Notifications/VerifyEmailQueued.php
@@ -10,4 +10,16 @@ class VerifyEmailQueued extends VerifyEmail implements ShouldQueue
 {
     use Queueable;
 
+    /**
+     * @TODO
+     * Add This Construct for Seinding Post Mark  smtp Mail
+     * if you are not interast with post mark you can remove this constact method
+     */
+
+    public function __construct()
+    {
+        $this->callbacks[] =(function ($message) {
+            $message->getHeaders()->addTextHeader('X-PM-Message-Stream', 'Transactional');
+        });
+    }
 }
