453c48195b2efdaae9ae9e7f1fccce6146973c93
Md. Anichur Rahaman <38912435+anis3139@users.noreply.github.com>
2023-01-29 15:32:59 +0600

    Fix: Css Issues

---
+++ resources/js/Components/Banner.vue
@@ -0,0 +1,63 @@
+<template>
+    <div
+        class="bg-cyan-50 text-gray-900 dark:bg-gray-900 dark:text-gray-50 pt-10 sm:pt-16 lg:overflow-hidden lg:pt-8 lg:pb-14"
+    >
+        <div class="mx-auto max-w-7xl lg:px-8">
+            <div class="lg:grid lg:grid-cols-2 lg:gap-8">
+                <div
+                    class="mx-auto max-w-md px-6 sm:max-w-2xl sm:text-center lg:flex lg:items-center lg:px-0 lg:text-left"
+                >
+                    <div class="lg:py-24">
+                        <a
+                            href="#"
+                            class="inline-flex items-center rounded-full p-1 pr-2 text-gray-900 dark:text-white bg-gray-200 dark:bg-black hover:text-gray-200 sm:text-base lg:text-sm xl:text-base"
+                        >
+                            <span
+                                class="rounded-full bg-gradient-to-r from-teal-500 to-cyan-600 px-3 py-0.5 text-sm font-semibold leading-5 text-white"
+                                >Contact us</span
+                            >
+                            <span class="ml-4 text-sm"
+                                >Visit to contact page</span
+                            >
+                            <font-awesome-icon
+                                icon="fa-solid fa-chevron-right "
+                                class="ml-2 h-3 mt-1 w-3 text-gray-500"
+                            />
+                        </a>
+                        <h1
+                            class="mt-4 text-4xl font-bold tracking-tight dark:text-white text-gray-800 sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl"
+                        >
+                            <span class="block">A better way to</span>
+                            <span
+                                class="block bg-gradient-to-r from-teal-200 to-cyan-400 bg-clip-text pb-3 text-transparent sm:pb-5"
+                                >ship web apps</span
+                            >
+                        </h1>
+                        <p
+                            class="text-base text-gray-500 dark:text-gray-300 sm:text-xl lg:text-lg xl:text-xl"
+                        >
+                            Anim aute id magna aliqua ad ad non deserunt sunt.
+                            Qui irure qui Lorem cupidatat commodo. Elit sunt
+                            amet fugiat veniam occaecat fugiat.
+                        </p>
+                    </div>
+                </div>
+                <div class="mt-12 -mb-16 sm:-mb-48 lg:relative lg:m-0">
+                    <div
+                        class="mx-auto max-w-md px-6 sm:max-w-2xl lg:max-w-none lg:px-0"
+                    >
+                        <img
+                            class="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+                            :src="$page.props.global.options.banner"
+                            alt=""
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup></script>
+
+<style></style>
---
+++ resources/js/Components/Header.vue
@@ -0,0 +1,83 @@
+<script setup>
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import Menu from "@/Components/Menu.vue";
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/vue";
+import { XMarkIcon } from "@heroicons/vue/24/outline";
+const navigation = [
+    { name: "Home", route: "home" },
+    { name: "About", route: "about" },
+    { name: "Products", route: "products" },
+    { name: "Contact", route: "contact" },
+];
+</script>
+<template>
+    <Popover as="header" class="relative">
+        <div class="bg-cyan-50 text-gray-900 dark:bg-gray-900 dark:text-gray-50 pt-6 pb-3">
+            <Menu :navigation="navigation"></Menu>
+        </div>
+
+        <transition
+            enter-active-class="duration-150 ease-out"
+            enter-from-class="opacity-0 scale-95"
+            enter-to-class="opacity-100 scale-100"
+            leave-active-class="duration-100 ease-in"
+            leave-from-class="opacity-100 scale-100"
+            leave-to-class="opacity-0 scale-95"
+        >
+            <PopoverPanel
+                focus
+                class="absolute inset-x-0 top-0 origin-top transform p-2 transition md:hidden"
+            >
+                <div
+                    class="overflow-hidden rounded-lg bg-white dark:bg-gray-800 shadow-md ring-1 ring-black ring-opacity-5"
+                >
+                    <div class="flex items-center justify-between px-5 pt-4">
+                        <div>
+                            <ApplicationLogo
+                                class="h-12 w-12"
+                            ></ApplicationLogo>
+                        </div>
+                        <div class="-mr-2">
+                            <PopoverButton
+                                class="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600"
+                            >
+                                <span class="sr-only">Close menu</span>
+                                <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+                            </PopoverButton>
+                        </div>
+                    </div>
+                    <div class="pt-5 pb-6">
+                        <div class="space-y-1 px-2">
+                            <Link
+                                v-for="item in navigation"
+                                :key="item.name"
+                                :href="route(item.route)"
+                                :class="
+                                    route().current() == item.route
+                                        ? 'block rounded-md px-3 py-2 text-base font-medium text-cyan-300   hover:text-cyan-600 underline decoration-cyan-300  underline-offset-4 '
+                                        : 'block rounded-md px-3 py-2 text-base font-medium text-gray-900 dark:text-gray-50 hover:text-cyan-300 hover:underline hover:decoration-cyan-300  hover:underline-offset-4'
+                                "
+                            >
+                                {{ item.name }}
+                            </Link>
+                        </div>
+                        <div class="mt-6 px-5">
+                            <Link
+                                v-if="$page.props.auth.user"
+                                :href="route('dashboard')"
+                                class="block w-full rounded-md bg-gradient-to-r from-teal-500 to-cyan-600 py-3 px-4 text-center font-medium text-white shadow hover:from-teal-600 hover:to-cyan-700"
+                                >Go Dashboard</Link
+                            >
+                            <Link
+                                v-else
+                                :href="route('login')"
+                                class="block w-full rounded-md bg-gradient-to-r from-teal-500 to-cyan-600 py-3 px-4 text-center font-medium text-white shadow hover:from-teal-600 hover:to-cyan-700 "
+                                >Log in</Link
+                            >
+                        </div>
+                    </div>
+                </div>
+            </PopoverPanel>
+        </transition>
+    </Popover>
+</template>
