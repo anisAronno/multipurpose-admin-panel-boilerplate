dcda3bbc9d5062f0ef6525e3e44a89cb817ae879
Md. Anichur Rahaman <38912435+anis3139@users.noreply.github.com>
2023-01-31 22:59:22 +0600

    Add Translet file in lang folder

---
+++ resources/lang/en/products.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    "product" =>[
+        'title' => 'Product',
+        'mobile_title' => 'Product',
+        'heading' => "All Products",
+        'description' => "Phasellus lorem quam molestie id quisque diam aenean nulla
+                    in. Accumsan in quis quis nunc, ullamcorper malesuada.
+                    Eleifend condimentum id viverra nulla.",
+    ],
+    "hero" =>[
+        'product' => 'Product Page'
+    ],
+
+    'special' => [
+        'feature'    =>   [
+            'title' =>  "Our Special Feature",
+            'description' =>  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        ],
+    ],
+    'featured' => [
+        'product'    =>   [
+            'title' =>  "Special Products For You",
+            'description' =>  "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        ],
+    ],
+
+];
