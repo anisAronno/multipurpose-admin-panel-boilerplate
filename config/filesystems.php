d5078944cd966126c36a5968dcbbd939dbc691a6
Md. Anichur Rahaman <anis904692@gmail.com>
2023-03-13 10:47:44 +0600

    Local Filesystem permission updated

--- config/filesystems.php
+++ config/filesystems.php
@@ -35,6 +35,16 @@ return [
             'root' => storage_path('app'),
             'throw' => false,
             'url' => env('APP_URL').'/storage',
+            'permissions' => [
+                'file' => [
+                    'public' => 0644,
+                    'private' => 0600,
+                ],
+                'dir' => [
+                    'public' => 0755,
+                    'private' => 0755,
+                ],
+             ],
         ],
 
         'public' => [
